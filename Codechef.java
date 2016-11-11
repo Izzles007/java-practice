654963772960dd477b009e29aced1cc40a19cb3d
Izzles007 <izzy.sowells@gmail.com>
2016-11-11 16:11:24 -0500

    Codechef.java

---
+++ Codechef.java
@@ -0,0 +1,19 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+   
+	String word ="Izzy";
+boolean isGirl;
+{System.out.println("hello world");
+if(isGirl==true);
+System.out.println("hello world");
+
+
+
+
